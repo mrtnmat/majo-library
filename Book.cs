8fb595c5ee7fd2660ce9ea4b64e2c5d23fd8e421
mrtnmat <47029619+mrtnmat@users.noreply.github.com>
2022-05-11 18:37:31 +0200

    Book class framework

--- Book.cs
+++ Book.cs
@@ -8,5 +8,24 @@ namespace majo_library
 {
     class Book
     {
+        string _title;
+        string _author;
+        public Book(string title, string author) => (_title, _author) = (title, author);
+
+        public string Title => _title;
+        public string Author => _author;
+        public override bool Equals(object obj)
+        {
+            return obj is Book && Equals((Book)obj);
+        }
+        public bool Equals(Book b)
+        {
+            return _title == b.Title && _author == b.Author;
+        }
+
+        public override int GetHashCode()
+        {
+            return (_title + _author).GetHashCode();
+        }
     }
 }

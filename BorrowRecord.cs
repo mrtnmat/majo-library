fbe440becde8939a241b58ea19ff0601a683e888
mrtnmat <47029619+mrtnmat@users.noreply.github.com>
2022-05-11 19:40:43 +0200

    Create BorrowRecord.cs

---
+++ BorrowRecord.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace majo_library
+{
+    class BorrowRecord
+    {
+        User _user;
+        BookData _book;
+
+        public User Lendee => _user;
+        public BookData BookLent => _book;
+
+        public bool Equals(BorrowRecord p)
+        {
+            return p.Lendee == Lendee && p.BookLent == BookLent;
+        }
+    }
+}
